8883d884836a56863d2d860ddbcac6c543bde865
Hackatosh <edouardbenauw@gmail.com>
2023-05-28 17:41:24 +0200

    Add more op codes

--- cpu/opsCode.go
+++ cpu/opsCode.go
@@ -95,8 +95,15 @@ var hexToOpsCode = map[uint8]OpCode{
 	0xA9: {operation: LDA, addressingMode: Immediate, bytes: 2, cycles: 2},
 	0xA5: {operation: LDA, addressingMode: ZeroPage, bytes: 2, cycles: 3},
 	0xAD: {operation: LDA, addressingMode: Absolute, bytes: 3, cycles: 4},
+	// Register instructions
 	0xAA: {operation: TAX, addressingMode: Implicit, bytes: 1, cycles: 2},
+	0x8A: {operation: TXA, addressingMode: Implicit, bytes: 1, cycles: 2},
+	0xCA: {operation: DEX, addressingMode: Implicit, bytes: 1, cycles: 2},
 	0xE8: {operation: INX, addressingMode: Implicit, bytes: 1, cycles: 2},
+	0xA8: {operation: TAY, addressingMode: Implicit, bytes: 1, cycles: 2},
+	0x98: {operation: TYA, addressingMode: Implicit, bytes: 1, cycles: 2},
+	0x88: {operation: DEY, addressingMode: Implicit, bytes: 1, cycles: 2},
+	0xC8: {operation: INY, addressingMode: Implicit, bytes: 1, cycles: 2},
 }
 
 func matchHexCodeWithOpsCode(hexCode uint8) OpCode {
