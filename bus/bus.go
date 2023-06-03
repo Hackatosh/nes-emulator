0b5bc0df7f2c04667a422864fd610a369f8d0de0
Hackatosh <edouardbenauw@gmail.com>
2023-06-03 13:21:25 +0200

    Finish to implement cartridge

--- bus/bus.go
+++ bus/bus.go
@@ -9,26 +9,40 @@ const CPU_RAM_START uint16 = 0x0000
 const CPU_RAM_MIRRORS_END uint16 = 0x1FFF
 const PPU_REGISTERS_START uint16 = 0x2000
 const PPU_REGISTERS_MIRRORS_END uint16 = 0x3FFF
+const PRG_ROM_START uint16 = 0x8000
+const PRG_ROM_END uint16 = 0xFFFF
 
 type Bus struct {
+	rom    Rom
 	memory [0xffff]uint8
 	// More info on memory structure here : https://www.nesdev.org/wiki/CPU_memory_map
 }
 
 // Memory helpers
 
+func (bus Bus) readPrgROM(address uint16) uint8 {
+	var unmirroredAddress = address - 0x8000
+	// Unmirroring if prgRom is of 16 KiB (we map 32 KiB addressing space)
+	if len(bus.rom.prgRom) == 0x4000 && address >= 0x4000 {
+		unmirroredAddress = address % 0x4000
+	}
+	return bus.rom.prgRom[unmirroredAddress]
+}
+
 func (bus Bus) MemoryRead(address uint16) uint8 {
 	var unmirroredAddress uint16
 	switch {
 	case CPU_RAM_START <= address && address <= CPU_RAM_MIRRORS_END:
 		unmirroredAddress = address & 0b00000111_11111111
+		return bus.memory[unmirroredAddress]
 	case PPU_REGISTERS_START <= address && address <= PPU_REGISTERS_MIRRORS_END:
 		unmirroredAddress = address & 0b00100000_00000111
+		return bus.memory[unmirroredAddress]
+	case PRG_ROM_START <= address && address <= PRG_ROM_END:
+		return bus.readPrgROM(address)
 	default:
 		panic(fmt.Sprintf("Unsupported address %v", address))
 	}
-
-	return bus.memory[unmirroredAddress]
 }
 
 func (bus Bus) MemoryWrite(address uint16, data uint8) {
@@ -38,6 +52,8 @@ func (bus Bus) MemoryWrite(address uint16, data uint8) {
 		unmirroredAddress = address & 0b00000111_11111111
 	case PPU_REGISTERS_START <= address && address <= PPU_REGISTERS_MIRRORS_END:
 		unmirroredAddress = address & 0b00100000_00000111
+	case PRG_ROM_START <= address && address <= PRG_ROM_END:
+		panic(fmt.Sprintf("Trying to write to address %v in PRG ROM", address))
 	default:
 		panic(fmt.Sprintf("Unsupported address %v", address))
 	}
@@ -64,8 +80,9 @@ func (bus Bus) LoadProgram(program []uint8) {
 	copy(bus.memory[0x8000:0xFFFF], program)
 }
 
-func NewBus() Bus {
+func NewBus(rom Rom) Bus {
 	return Bus{
+		rom:    rom,
 		memory: [0xffff]uint8{},
 	}
 }
